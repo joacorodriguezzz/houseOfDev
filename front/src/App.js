440b9878fc381f041cf62c19e6aacd13c0c2c503
fsosa44 <sosafrancisco2005@gmail.com>
2023-11-14 20:23:59 -0300

    'Register process, falta conectar back con front'

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,12 @@ import Register from "./Register";
 import Home from "./Home";
 import { Navigate, Route, Routes } from "react-router";
 import { useState } from "react";
+import Login from "./Login";
 
 function App() {
   const [user, setUser] = useState([]);
   return (
-    <div>
-      <Register setUser={setUser} />
-      <Home />
-    </div>
+    <div>{!user.length > 0 ? <Register setUser={setUser} /> : <Login />}</div>
   );
 }
 
