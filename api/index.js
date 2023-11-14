b5e75e859ad9791606e74da94f0c6d4e5f84510f
joacorodriguezzz <joaquinrodriguezz225@gmail.com>
2023-11-14 14:40:15 -0300

    dbsync

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const db = require("./configs/db");
+const models = require("./models");
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.listen(3000, () => {
-  console.log("Escuchando al puerto 3000");
-});
+db.sync({ force: false })
+  .then(function () {
+    // Recién ahora estamos seguros que la conexión fue exitosa
+    app.listen(3001, () =>
+      console.log("Servidor escuchando en el puerto 3001")
+    );
+  })
+  .catch(console.error);
---
+++ api/models/User.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../configs/db");
+
+class User extends Model {}
+User.init(
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    salt: {
+      type: DataTypes.STRING,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: sequelize,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
