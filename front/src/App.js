a0c32238b3c03427904d998366d6ccc57090ff66
joacorodriguezzz <joaquinrodriguezz225@gmail.com>
2023-11-22 17:41:21 -0300

    Sprint 2 in process

--- front/src/App.js
+++ front/src/App.js
@@ -6,20 +6,26 @@ import { useState } from "react";
 import Login from "./pages/Login";
 import { AuthProvider } from "./context/user";
 import Logged from "./pages/Logged";
+import NavBar from "./components/NavBar";
+import InmContainer from "./components/inmContainer";
 
 function App() {
   const [user, setUser] = useState([]);
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register setUser={setUser} />} />
-          <Route path="/logIn" element={<Login />} />
-          <Route path="/home" element={<Logged />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
+    <div>
+      <NavBar />
+      <InmContainer />
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register setUser={setUser} />} />
+            <Route path="/logIn" element={<Login />} />
+            <Route path="/home" element={<Logged />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 }
 
