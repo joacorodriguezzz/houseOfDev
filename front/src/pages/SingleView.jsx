00d6bf82a62c3e6a93a331511c6b8fd016ab99bc
joacorodriguezzz <joaquinrodriguezz225@gmail.com>
2023-11-28 15:38:38 -0300

    Single view almost finished

--- front/src/pages/SingleView.jsx
+++ front/src/pages/SingleView.jsx
@@ -35,7 +35,7 @@ function SingleView() {
       )}
       <div class="">
         <p class="mt-8 font-serif text-xl font-bold text-blue-900">
-          Select a date
+          Seleccione una fecha
         </p>
         <div class="relative mt-4 w-56">
           <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
@@ -59,39 +59,71 @@ function SingleView() {
             autofocus="autofocus"
             type="text"
             class="datepicker-input block w-full rounded-lg border border-red-300 bg-red-50 p-2.5 pl-10 text-red-800 outline-none ring-opacity-30 placeholder:text-emerald-800 focus:ring focus:ring-emerald-300 sm:text-sm"
-            placeholder="Select date"
+            placeholder="Seleccione una fecha"
           />
         </div>
       </div>
 
       <div class="">
         <p class="mt-8 font-serif text-xl font-bold text-blue-900">
-          Select a time
+          Seleccione un horario
         </p>
+
         <div class="mt-4 grid grid-cols-4 gap-2 lg:max-w-xl">
-          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
-            12:00
-          </button>
-          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
-            14:00
-          </button>
-          <button class="rounded-lg bg-red-700 px-4 py-2 font-medium text-white active:scale-95">
+          <button
+            id="btn"
+            class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95 hover:bg-red-300 focus:bg-red-600 focus:text-white"
+          >
             09:00
           </button>
-          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
+
+          <button
+            id="btn"
+            class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95 hover:bg-red-300 focus:bg-red-600 focus:text-white"
+          >
+            11:00
+          </button>
+
+          <button
+            id="btn"
+            class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95 hover:bg-red-300 focus:bg-red-600 focus:text-white"
+          >
             12:00
           </button>
-          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
-            15:00
+
+          <button
+            id="btn"
+            class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95 hover:bg-red-300 focus:bg-red-600 focus:text-white"
+          >
+            13:00
           </button>
-          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
-            12:00
+
+          <button
+            id="btn"
+            class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95 hover:bg-red-300 focus:bg-red-600 focus:text-white"
+          >
+            16:00
           </button>
-          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
-            14:00
+
+          <button
+            id="btn"
+            class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95 hover:bg-red-300 focus:bg-red-600 focus:text-white"
+          >
+            17:00
           </button>
-          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
-            12:00
+
+          <button
+            id="btn"
+            class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95 hover:bg-red-300 focus:bg-red-600 focus:text-white"
+          >
+            18:00
+          </button>
+
+          <button
+            id="btn"
+            class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95 hover:bg-red-300 focus:bg-red-600 focus:text-white"
+          >
+            19:00
           </button>
         </div>
       </div>
