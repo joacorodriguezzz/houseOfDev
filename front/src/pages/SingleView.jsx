2023b4f23387456a0490850b9d9f983f62ad5ff3
joacorodriguezzz <joaquinrodriguezz225@gmail.com>
2023-11-28 14:44:06 -0300

    Appointment views in process

--- front/src/pages/SingleView.jsx
+++ front/src/pages/SingleView.jsx
@@ -33,6 +33,72 @@ function SingleView() {
       ) : (
         <p>Cargando...</p>
       )}
+      <div class="">
+        <p class="mt-8 font-serif text-xl font-bold text-blue-900">
+          Select a date
+        </p>
+        <div class="relative mt-4 w-56">
+          <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg
+              aria-hidden="true"
+              class="h-5 w-5 text-gray-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            datepicker=""
+            datepicker-orientation="bottom"
+            autofocus="autofocus"
+            type="text"
+            class="datepicker-input block w-full rounded-lg border border-red-300 bg-red-50 p-2.5 pl-10 text-red-800 outline-none ring-opacity-30 placeholder:text-emerald-800 focus:ring focus:ring-emerald-300 sm:text-sm"
+            placeholder="Select date"
+          />
+        </div>
+      </div>
+
+      <div class="">
+        <p class="mt-8 font-serif text-xl font-bold text-blue-900">
+          Select a time
+        </p>
+        <div class="mt-4 grid grid-cols-4 gap-2 lg:max-w-xl">
+          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
+            12:00
+          </button>
+          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
+            14:00
+          </button>
+          <button class="rounded-lg bg-red-700 px-4 py-2 font-medium text-white active:scale-95">
+            09:00
+          </button>
+          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
+            12:00
+          </button>
+          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
+            15:00
+          </button>
+          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
+            12:00
+          </button>
+          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
+            14:00
+          </button>
+          <button class="rounded-lg bg-red-100 px-4 py-2 font-medium text-red-900 active:scale-95">
+            12:00
+          </button>
+        </div>
+      </div>
+
+      <button class="mt-8 w-56 rounded-full border-8 border-red-500 bg-red-600 px-10 py-4 text-lg font-bold text-white transition hover:translate-y-1">
+        Book Now
+      </button>
     </div>
   );
 }
